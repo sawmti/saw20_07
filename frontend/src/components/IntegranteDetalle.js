9f40e8c7c10764e71e65e1952aa288b3becbc2fe
Jorge Vega <jorge@pegasconsentido.cl>
2021-07-30 17:58:19 -0400

    Se agrega logica de lectura de datos para detalle

--- frontend/src/components/IntegranteDetalle.js
+++ frontend/src/components/IntegranteDetalle.js
@@ -2,86 +2,90 @@ import React, { Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import clienteAxios from '../config/axios';
 
-const IntegranteDetalle = (props) => {
-
-    if (!props.integrante){
-        props.history.push('/');
-        return null;
+class IntegranteDetalle extends React.Component {
+//const  = (props) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.integranteid,
+            detalle: {
+                content: {}
+            }
+        };
     }
 
-    // extraer por props
-    //const { integrante: { nombre, codexterno, ocupacion, urlimagen } } = props;
+    componentDidMount() {
+        clienteAxios.get('/integrantes/'+this.state.id)
+        .then(respuesta => {
+            this.setState({ detalle: respuesta.data.data.integrante });
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
 
-    //console.log(props.integrante.data);
-    //console.log(props.integranteid);
+    render() {
+        return( 
+            <Fragment>
+                <section className="py-4 text-center container">
+                    <div className="row py-lg-5">
+                        <div className="col-lg-6 col-md-8 mx-auto">
+                            <h1 className="fw-light">Integrante: </h1>
 
-    let detalle;
-
-    clienteAxios.get('/integrantes/'+props.integranteid)
-    .then(respuesta => {
-        //console.log(respuesta.data.data.integrante)
-        detalle = respuesta.data.data.integrante;
-        console.log(detalle);
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
-    console.log(detalle);
-
-    return( 
-        <Fragment>
-            <section className="py-4 text-center container">
-                <div className="row py-lg-5">
-                    <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light">Integrante: </h1>
-
-                        <p className="lead text-muted">En esta sección se muestra el detalle
-                        del integrante de la convención seleccionado.
-                        </p>
-                        <p>
-                            <Link to={'/buscar'} className="btn btn-primary my-2">Buscar integrante</Link>
-                            &nbsp;
-                            <Link to={'/comentarios'} className="btn btn-secondary my-2">Envíanos tus comentarios</Link>
-                            &nbsp;
-                            <Link to={'/agregar'} className="btn btn-success my-2">Agregar integrante</Link>
-                        </p>
+                            <p className="lead text-muted">En esta sección se muestra el detalle
+                            del integrante de la convención seleccionado.
+                            </p>
+                            <p>
+                                <Link to={'/buscar'} className="btn btn-primary my-2">Buscar integrante</Link>
+                                &nbsp;
+                                <Link to={'/comentarios'} className="btn btn-secondary my-2">Envíanos tus comentarios</Link>
+                                &nbsp;
+                                <Link to={'/agregar'} className="btn btn-success my-2">Agregar integrante</Link>
+                            </p>
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-            <div className="container px-4 py-4" id="icon-grid">
-                <h2 className="pb-2 border-bottom">Detalle</h2>
+                <div className="container px-4 py-4" id="icon-grid">
+                    <h2 className="pb-2 border-bottom">Detalle</h2>
 
-                <div className="row featurette">
-                    <div className="col-md-8">
+                    <div className="row featurette">
+                        <div className="col-md-8">
 
-                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 g-4 py-5">
-                            <div className="col d-flex align-items-start">
-                                <i className="fas fa-code iconoportal"></i>
-                                <div>
-                                <h4 className="fw-bold mb-0">Código WikiData</h4>
-                                <p>codexterno</p>
+                            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 g-4 py-5">
+                                <div className="col d-flex align-items-start">
+                                    <i className="fas fa-code iconoportal"></i>
+                                    <div>
+                                    <h4 className="fw-bold mb-0">Código WikiData</h4>
+                                    <p>{this.state.detalle.entity}</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col d-flex align-items-start">
-                                <i className="fas fa-briefcase iconoportal"></i>
-                                <div>
-                                <h4 className="fw-bold mb-0">Ocupación</h4>
-                                <p>ocupacion</p>
+                                <div className="col d-flex align-items-start">
+                                    <i className="fas fa-briefcase iconoportal"></i>
+                                    <div>
+                                    <h4 className="fw-bold mb-0">Ocupación</h4>
+                                    <p>{this.state.detalle.content.occupation}</p>
+                                    </div>
+                                </div>
+                                <div className="col d-flex align-items-start">
+                                    <i className="fas fa-briefcase iconoportal"></i>
+                                    <div>
+                                    <h4 className="fw-bold mb-0">Posicion</h4>
+                                    <p>{this.state.detalle.content.position}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div className="col-md-4">
+                            <img src={this.state.detalle.content.image} className="card-img-top" alt="nombre" />
+                        </div>
                     </div>
-                    <div className="col-md-4">
-                        <img src="urlimagen" className="card-img-top" alt="nombre" />
-                    </div>
+
                 </div>
 
-            </div>
-
-        </Fragment>
-    );
+            </Fragment>
+        );
+    }
 }
 
 export default withRouter(IntegranteDetalle);
